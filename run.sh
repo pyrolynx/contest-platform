fcdc3046296bdae9c99a9166ae137ef318b32dba
Чуркин Кирилл Сергеевич <k-churkin@tochka.com>
2019-06-03 14:22:08 +0500

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t contest-platform .
-docker run -d -p 5000:5000 -v data:/opt/data contest-platform
+docker run -d -p 5000:5000 -v $PWD/data:/opt/data contest-platform
